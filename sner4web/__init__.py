23a52f278bb1c9f70a6d962c93a60d852de6590a
root <root@ztook2.meta.zcu.cz>
2018-10-07 11:15:14 +0200

    pytest to requirements flask-debugtoolbar

--- sner4web/__init__.py
+++ sner4web/__init__.py
@@ -1,9 +1,15 @@
 from flask import flash, Flask, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+
+toolbar = DebugToolbarExtension()
 
 
 def create_app(config_file="sner4web.cfg"):
 	app = Flask(__name__)
 	app.config.from_pyfile(config_file)
+
+	toolbar.init_app(app)
 
 	@app.route("/")
 	def index():
