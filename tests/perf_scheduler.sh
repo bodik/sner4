781ae3b3503bd601b93066686c29b2d2d78940dd
Radoslav Bodo <bodik@cesnet.cz>
2019-02-27 21:33:11 +0100

    scheduler: new pert tester

--- tests/perf_scheduler.sh
+++ tests/perf_scheduler.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+
+ITERATIONS=4
+if [ -n "$1" ]; then
+	ITERATIONS=$1
+fi
+
+
 echo 'INFO: database cleanup'
 sh bin/server_make_db.sh 1>/dev/null 2>/dev/null
 for all in $(sh bin/server_command.sh scheduler task_list | awk '{print $1}' | grep -v id); do
@@ -8,18 +15,18 @@ for all in $(sh bin/server_command.sh scheduler task_list | awk '{print $1}' | g
 done
 
 
-echo -n 'INFO: task add: '
-TESTNET="$(seq 1 3 | xargs -I{} printf '10.%d.0.0/16 ' {})"
-time sh -c "(sh bin/server_command.sh scheduler enumips $TESTNET | sh bin/server_command.sh scheduler task_add 1 --file - --name 'testnet' --group_size 100)"
+for iter in $(seq 1 ${ITERATIONS}); do
+	echo "INFO: test iteration ${iter}"
+	echo -en "INFO: task add: \t\t"
+	TESTNET=$(printf '10.%d.0.0/16 11.%d.0.0/16 12.%d.0.0/16' ${iter} ${iter} ${iter})
+	(time sh -c "(sh bin/server_command.sh scheduler enumips ${TESTNET} | sh bin/server_command.sh scheduler task_add 1 --file - --name 'testnet' --group_size 100)") 2>&1 | grep real
 
+	echo -en "INFO: task list: \t\t"
+	(time sh bin/server_command.sh scheduler task_list 1>/dev/null) 2>&1 | grep real
 
-echo -n 'INFO: task list: '
-time sh bin/server_command.sh scheduler task_list 1>/dev/null
+	echo -en "INFO: task schedule: \t\t"
+	(time sh bin/server_command.sh scheduler task_targets $((2+${iter})) schedule) 2>&1 | grep real
 
-
-echo -n 'INFO: task schedule: '
-time sh bin/server_command.sh scheduler task_targets 3 schedule
-
-
-echo -n 'INFO: job/assign: '
-time curl -o /dev/null -s http://localhost:18000/scheduler/job/assign/3
+	echo -en "INFO: job/assign: \t\t"
+	(time curl -o /dev/null -s http://localhost:18000/scheduler/job/assign/3) 2>&1 | grep real
+done
