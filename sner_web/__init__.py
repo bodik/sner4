f3234f4fc094aac2c419b4c756dc79730a82a35b
Radoslav Bodo <bodik@cesnet.cz>
2019-02-27 21:33:09 +0100

    code cosmetics

--- sner_web/__init__.py
+++ sner_web/__init__.py
@@ -7,28 +7,27 @@ from sner_web.extensions import db, toolbar
 from sner_web.models import Profile, Task
 
 
-
 def create_app():
 	"""flask application factory"""
 
 	app = Flask(__name__)
-	app.config.from_envvar("SNER_CONFIG")
+	app.config.from_envvar('SNER_CONFIG')
 
 	toolbar.init_app(app)
 	db.init_app(app)
 
-	app.register_blueprint(profile.blueprint, url_prefix="/profile")
-	app.register_blueprint(task.blueprint, url_prefix="/task")
+	app.register_blueprint(profile.blueprint, url_prefix='/profile')
+	app.register_blueprint(task.blueprint, url_prefix='/task')
 
 	app.cli.add_command(init_db)
 	app.cli.add_command(init_data)
 
-	@app.route("/")
+	@app.route('/')
 	def index_route(): # pylint: disable=unused-variable
-		flash("info", "info")
-		flash("success", "success")
-		flash("warning", "warning")
-		flash("error", "error")
-		return render_template("index.html")
+		flash('info', 'info')
+		flash('success', 'success')
+		flash('warning', 'warning')
+		flash('error', 'error')
+		return render_template('index.html')
 
 	return app
