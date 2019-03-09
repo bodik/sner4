cfe324c95007e7f1246141f54a01652100491d27
Radoslav Bodo <bodik@cesnet.cz>
2019-03-09 22:14:09 +0100

    storage: (EXPERIMENTAL) hostnames visualization

--- sner/server/controller/storage/host.py
+++ sner/server/controller/storage/host.py
@@ -28,3 +28,43 @@ def host_delete_route(host_id):
 		db.session.commit()
 		return redirect(url_for('storage.host_list_route'))
 	return render_template('button_delete.html', form=form, form_url=url_for('storage.host_delete_route', host_id=host_id))
+
+
+@blueprint.route('/host/vizdns')
+def host_vizdns_route():
+	crop = request.args.get('crop', 1, type=int)
+	return render_template('storage/host/vizdns.html', crop=crop)
+
+
+@blueprint.route('/host/vizdns.json')
+def host_vizdns_json_route():
+
+	## from all hostnames we know, create tree structure dict-of-dicts
+	def to_tree(node, items):
+		if not items:
+			return {}
+		if items[0] not in node:
+			node[items[0]] = {}
+		node[items[0]] = to_tree(node[items[0]], items[1:])
+		return node
+
+	crop = request.args.get('crop', 1, type=int)
+	hostnames_tree = {}
+	for ihost in Host.query.all():
+		if ihost.hostname:
+			hostnames_tree = to_tree(hostnames_tree, list(reversed(ihost.hostname.split('.')[crop:])))
+
+
+	## walk through the tree and generate list of nodes and links
+	def to_graph_data(parentid, treedata, nodes, links):
+		for node in treedata:
+			nodeid = len(nodes)
+			nodes.append({'name': node, 'id': nodeid})
+			if parentid is not None:
+				links.append({'source': parentid, 'target': nodeid})
+			(nodes, links) = to_graph_data(nodeid, treedata[node], nodes, links)
+		return (nodes, links)
+
+	(nodes, links) = to_graph_data(None, hostnames_tree, [], [])
+
+	return jsonify({'nodes': nodes, 'links': links})
