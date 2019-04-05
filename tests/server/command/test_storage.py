817e8afc1425971422c5176bda68d1822415bb85
Radoslav Bodo <bodik@cesnet.cz>
2019-04-05 22:30:21 +0200

    parser: basic nessus parser testsuite

--- tests/server/command/test_storage.py
+++ tests/server/command/test_storage.py
@@ -7,7 +7,7 @@ import pytest
 
 from sner.server import db
 from sner.server.command.storage import storage_command
-from sner.server.model.storage import Host, Note
+from sner.server.model.storage import Host, Note, Vuln
 
 
 def test_import_nmap_command(runner):
@@ -22,7 +22,28 @@ def test_import_nmap_command(runner):
 	assert host.os == 'Linux 3.8 - 4.6'
 	assert sorted([x.port for x in host.services]) == [22, 25, 139, 445, 5432]
 	tmpnote = Note.query.filter(Note.host == host, Note.ntype == 'nmap.smtp-commands.tcp.25').one_or_none()
-	assert "PIPELINING" in json.loads(tmpnote.data)["output"]
+	assert 'PIPELINING' in json.loads(tmpnote.data)['output']
+
+
+	db.session.delete(host)
+	db.session.commit()
+
+
+def test_import_nessus_command(runner):
+	"""test nessus parser"""
+
+	#TODO: remove forcing parser type after implementing autodetection
+	result = runner.invoke(storage_command, ['import', 'tests/server/data/parser-nessus-simple.xml', '--parser', 'nessus'])
+	assert result.exit_code == 0
+
+	host_id = re.search(r'parsed host: <Host (?P<hostid>\d+):', result.output).group('hostid')
+	host = Host.query.filter(Host.id == host_id).one_or_none()
+	assert host
+	assert host.os == 'Microsoft Windows Vista'
+	assert sorted([x.port for x in host.services]) == [443]
+	tmpvuln = Vuln.query.filter(Vuln.host == host, Vuln.xtype == 'nessus.104631').one_or_none()
+	assert tmpvuln
+	assert 'CVE-1900-0000' in tmpvuln.refs
 
 
 	db.session.delete(host)
