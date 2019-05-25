11dc95fdb940518539b68f5d50f227fb33b0e73e
Radoslav Bodo <bodik@cesnet.cz>
2019-05-25 11:19:23 +0000

    tests: agent split tests

--- tests/agent/test_agent.py
+++ tests/agent/test_agent.py
@@ -1,9 +1,7 @@
-"""agent processhandling tests"""
+"""agent basic tests"""
 
 import json
-import multiprocessing
 import os
-import time
 from uuid import uuid4
 from zipfile import ZipFile
 
@@ -41,37 +39,3 @@ def test_run_with_liveserver(tmpworkdir, live_server, test_dummy_target):
     with ZipFile(job_output_filename(job.id)) as ftmp_zip:
         with ftmp_zip.open('assignment.json') as ftmp:
             assert test_dummy_target.target in ftmp.read().decode('utf-8')
-
-
-def test_terminate_with_assignment(tmpworkdir, cleanup_markedprocess, test_longrun_a):
-    """agent's external process handling test"""
-
-    proc_agent = multiprocessing.Process(target=agent_main, args=(['--assignment', json.dumps(test_longrun_a), '--debug'],))
-    proc_agent.start()
-    time.sleep(1)
-    assert proc_agent.pid
-    assert proc_agent.is_alive()
-
-    agent_main(['--terminate', str(proc_agent.pid)])
-    time.sleep(3)
-    assert 'MARKEDPROCESS' not in os.popen('ps -f').read()
-    assert not proc_agent.is_alive()
-    proc_agent.join()
-
-
-def test_terminate_with_liveserver(tmpworkdir, live_server, cleanup_markedprocess, test_longrun_target):
-    """agent's external process handling test"""
-
-    proc_agent = multiprocessing.Process(
-        target=agent_main,
-        args=(['--server', live_server.url(), '--debug', '--queue', str(test_longrun_target.queue_id), '--oneshot'],))
-    proc_agent.start()
-    time.sleep(1)
-    assert proc_agent.pid
-    assert proc_agent.is_alive()
-
-    agent_main(['--terminate', str(proc_agent.pid)])
-    time.sleep(3)
-    assert 'MARKEDPROCESS' not in os.popen('ps -f').read()
-    assert not proc_agent.is_alive()
-    proc_agent.join()
---
+++ tests/agent/test_processhandling.py
@@ -0,0 +1,42 @@
+"""agents process handling"""
+
+import json
+import multiprocessing
+import os
+import time
+
+from sner.agent import main as agent_main
+
+
+def test_terminate_with_assignment(tmpworkdir, cleanup_markedprocess, test_longrun_a):
+    """agent's external process handling test"""
+
+    proc_agent = multiprocessing.Process(target=agent_main, args=(['--assignment', json.dumps(test_longrun_a), '--debug'],))
+    proc_agent.start()
+    time.sleep(1)
+    assert proc_agent.pid
+    assert proc_agent.is_alive()
+
+    agent_main(['--terminate', str(proc_agent.pid)])
+    time.sleep(3)
+    assert 'MARKEDPROCESS' not in os.popen('ps -f').read()
+    assert not proc_agent.is_alive()
+    proc_agent.join()
+
+
+def test_terminate_with_liveserver(tmpworkdir, live_server, cleanup_markedprocess, test_longrun_target):
+    """agent's external process handling test"""
+
+    proc_agent = multiprocessing.Process(
+        target=agent_main,
+        args=(['--server', live_server.url(), '--debug', '--queue', str(test_longrun_target.queue_id), '--oneshot'],))
+    proc_agent.start()
+    time.sleep(1)
+    assert proc_agent.pid
+    assert proc_agent.is_alive()
+
+    agent_main(['--terminate', str(proc_agent.pid)])
+    time.sleep(3)
+    assert 'MARKEDPROCESS' not in os.popen('ps -f').read()
+    assert not proc_agent.is_alive()
+    proc_agent.join()
