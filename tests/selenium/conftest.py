d567e9391540ab50de62f0941fd6979baa7c35ad
Radoslav Bodo <bodik@cesnet.cz>
2019-06-01 09:55:07 +0000

    tests: selenium tests 	* initial selenium skeleton 	* selenium tests for storage ui 	* use no_ajax_pending() when firing ajax after alert confirmation; contributed by: robert.kopecky@igalileo.cz, jiri.prochazka@igalileo.cz

---
+++ tests/selenium/conftest.py
@@ -0,0 +1,13 @@
+"""shared fixtures for selenium tests"""
+
+import pytest
+
+from tests.server.model.storage import test_host, test_note, test_service, test_vuln  # noqa: F401  pylint: disable=unused-import
+
+
+@pytest.fixture
+def firefox_options(firefox_options):  # pylint: disable=redefined-outer-name
+    """override firefox options"""
+
+    firefox_options.headless = True
+    return firefox_options
---
+++ tests/selenium/test_default.py
@@ -0,0 +1,10 @@
+"""selenium based UI tests"""
+
+from flask import url_for
+
+
+def test_index(live_server, selenium):  # pylint: disable=unused-argument
+    """very basic index hit test"""
+
+    selenium.get(url_for('index_route', _external=True))
+    assert 'sner4 -' in selenium.title
