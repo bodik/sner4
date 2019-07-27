8732dbc644246e67c90ff78652adece5a95c5541
Radoslav Bodo <bodik@cesnet.cz>
2019-07-27 13:50:57 +0000

    wsgi: wsgi integration test helper

---
+++ tests/test_wsgi_integration.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -e
+
+apt-get -y install git sudo make postgresql-all
+apt-get -y install apache2 libapache2-mod-wsgi-py3
+
+make venv
+. venv/bin/activate
+
+make install-deps
+
+sudo -u postgres scripts/database_create.sh sner sner
+sudo -u postgres psql -c "ALTER USER sner WITH ENCRYPTED PASSWORD 'password';"
+mkdir -p /var/sner
+chown www-data /var/sner
+
+cat > /etc/sner.yaml <<_EOF_
+server:
+    secret: 'testsecret'
+    application_root: '/sner'
+    db: 'postgresql://sner:password@localhost/sner'
+_EOF_
+make db
+
+cp wsgi.conf.example /etc/apache2/conf-enabled/sner-wsgi.conf
+/etc/init.d/apache2 restart
+
+curl http://localhost/sner/
