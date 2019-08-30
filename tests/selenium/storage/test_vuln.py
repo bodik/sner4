a4196352f2c7c99156e732d3998243b00ddec33a
Radoslav Bodo <bodik@cesnet.cz>
2019-08-30 11:02:10 +0000

    server: tagging features for vuln_view_route

--- tests/selenium/storage/test_vuln.py
+++ tests/selenium/storage/test_vuln.py
@@ -5,6 +5,7 @@ selenium ui tests for storage.vuln component
 
 from flask import url_for
 
+from sner.server import db
 from sner.server.model.storage import Vuln
 from tests.selenium import dt_inrow_delete, dt_rendered, dt_wait_processing
 from tests.selenium.storage import check_select_rows, check_vulns_multiactions
@@ -84,6 +85,22 @@ def test_vuln_list_route_filtering(live_server, sl_operator, test_vulns_filterin
 
     sl_operator.get(url_for('storage.vuln_list_route', _external=True))
     check_vulns_filtering(sl_operator, 'vuln_list_table')
+
+
+def test_vuln_view_route_tagging(live_server, sl_operator, test_vuln):  # pylint: disable=unused-argument
+    """test vuln view tagging features"""
+
+    sl_operator.get(url_for('storage.vuln_view_route', vuln_id=test_vuln.id, _external=True))
+
+    sl_operator.find_element_by_xpath('//a[contains(@class, "abutton_vuln_tag_set") and contains(text(), "Info")]').click()
+    assert sl_operator.find_elements_by_xpath('//span[contains(@class, "label") and contains(text(), "info")]')
+    vuln = Vuln.query.get(test_vuln.id)
+    assert 'info' in vuln.tags
+
+    sl_operator.find_element_by_xpath('//span[contains(@class, "tag-label") and contains(text(), "info")]/a[@class="abutton_vuln_tag_unset"]').click()
+    assert not sl_operator.find_elements_by_xpath('//span[contains(@class, "tag-label") and contains(text(), "info")]')
+    db.session.refresh(vuln)
+    assert 'info' not in vuln.tags
 
 
 def test_vuln_grouped_route(live_server, sl_operator, test_vuln):  # pylint: disable=unused-argument
