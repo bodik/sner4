b38e0c3fd74b6b6620454c7d2f6c673d2258f8fe
Radoslav Bodo <bodik@cesnet.cz>
2020-03-27 22:12:30 +0100

    server: scheduler job view show queue ident

--- sner/server/scheduler/views/job.py
+++ sner/server/scheduler/views/job.py
@@ -35,8 +35,7 @@ def job_list_json_route():
 
     columns = [
         ColumnDT(Job.id, mData='id'),
-        ColumnDT(Queue.id, mData='queue_id'),
-        ColumnDT(Queue.name, mData='queue_name'),
+        ColumnDT(Queue.ident, mData='queue_ident'),
         ColumnDT(Job.assignment, mData='assignment'),
         ColumnDT(Job.retval, mData='retval'),
         ColumnDT(Job.time_start, mData='time_start'),
