888c5cdf7fe8b2297c825518f9a43ed3194deb7c
Radoslav Bodo <bodik@cesnet.cz>
2020-08-24 23:09:53 +0200

    agent: fix test for missing ipv6 travis

--- tests/agent/test_module_six_enum_discover.py
+++ tests/agent/test_module_six_enum_discover.py
@@ -4,6 +4,7 @@ agent module six_enum_discover
 """
 
 import json
+import os
 from uuid import uuid4
 
 from sner.agent.core import main as agent_main
@@ -16,5 +17,11 @@ def test_basic(tmpworkdir):  # pylint: disable=unused-argument
     test_a = {'id': str(uuid4()), 'config': {'module': 'six_enum_discover', 'rate': 100}, 'targets': ['::0', '::1']}
 
     result = agent_main(['--assignment', json.dumps(test_a), '--debug'])
+
+    # travis does not support ipv6 on bionic
+    if 'TRAVIS' in os.environ:
+        assert result == 1
+        return
+
     assert result == 0
     assert '::1' in file_from_zip(f'{test_a["id"]}.zip', 'output-1.txt').decode('utf-8')
