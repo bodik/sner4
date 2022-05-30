6cebe3866bc38f6a16dc9d27f9c23d9d7ff1223b
Radoslav Bodo <bodik@cesnet.cz>
2022-05-30 14:56:48 +0200

    server: cleanup homepage

--- tests/selenium/test_default.py
+++ tests/selenium/test_default.py
@@ -10,4 +10,4 @@ def test_index_route(live_server, selenium):  # pylint: disable=unused-argument
     """very basic index hit test"""
 
     selenium.get(url_for('index_route', _external=True))
-    assert 'Main page - sner4' in selenium.title
+    assert 'Homepage - sner4' in selenium.title
