765d9dcf4aeabd47c4dd3a2c94e057ec0d298463
Radoslav Bodo <bodik@cesnet.cz>
2022-06-23 23:58:22 +0200

    tests: update heavy ratelimit helper to apiv2

--- tests/test_heavy_ratelimit.sh
+++ tests/test_heavy_ratelimit.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # This file is part of sner4 project governed by MIT license, see the LICENSE.txt file.
 
+SERVER=$1
+
 APIKEY=$(bin/server auth add-agent | rev | awk '{print $1}' | rev)
-while true; do time curl -H "Authorization: Apikey $APIKEY" http://localhost:18000/api/scheduler/job/assign | grep '{}' && break; done
+while true; do time curl -XPOST -H "X-API-KEY: $APIKEY" "${SERVER}/api/v2/scheduler/job/assign" | grep '{}' && break; done
