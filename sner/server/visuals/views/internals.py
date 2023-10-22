1aa022f91e4aeb293415b90ee25cf11fe5175c4f
Radoslav Bodo <bodik@cesnet.cz>
2023-10-22 19:23:42 +0200

    server: heatmap_check to internals

--- sner/server/visuals/views/internals.py
+++ sner/server/visuals/views/internals.py
@@ -7,6 +7,7 @@ from flask import render_template
 
 from sner.server.api.core import get_metrics
 from sner.server.auth.core import session_required
+from sner.server.scheduler.core import SchedulerService
 from sner.server.visuals.views import blueprint
 
 
@@ -15,4 +16,8 @@ from sner.server.visuals.views import blueprint
 def internals_route():
     """show various internals"""
 
-    return render_template('visuals/internals.html', metrics=get_metrics())
+    return render_template(
+        'visuals/internals.html',
+        metrics=get_metrics(),
+        heatmap_check=SchedulerService.heatmap_check()
+    )
