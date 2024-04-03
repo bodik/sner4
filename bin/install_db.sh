ddc2fede095251e42e609a87fece3a33cadc168f
Radoslav Bodo <bodik@cesnet.cz>
2024-04-03 13:29:23 +0000

    server: fix postgresql privs for debian bookworm
    snerstack backport

--- bin/install_db.sh
+++ bin/install_db.sh
@@ -5,8 +5,10 @@ systemctl start postgresql
 
 sudo -u postgres psql -c "CREATE USER ${USER}" | true
 sudo -u postgres psql -c "CREATE DATABASE sner" | true
+sudo -u postgres psql -c "GRANT ALL ON SCHEMA public to ${USER}" sner | true
 mkdir -p /var/lib/sner
 chown www-data /var/lib/sner
 
 sudo -u postgres psql -c "CREATE DATABASE sner_test" | true
+sudo -u postgres psql -c "GRANT ALL ON SCHEMA public to ${USER}" sner_test | true
 mkdir -p /tmp/sner_test_var
